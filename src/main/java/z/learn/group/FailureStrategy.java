c1e4a2589571c4e9befe2637ae01c5918b71b6f2
chouyua <logsoi@126.com>
2018-07-22 17:28:52 +0800

    define interface

--- src/main/java/z/learn/group/FailureStrategy.java
+++ src/main/java/z/learn/group/FailureStrategy.java
@@ -4,6 +4,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 interface FailureStrategy<K, V> {
+
+    void onStartup(KafkaConsumer<K, V> consumer, ConsumeCallback<K, V> callback);
+
+    void onShutdown(KafkaConsumer<K, V> consumer, ConsumeCallback<K, V> callback);
+
     void onCommitFailure(KafkaConsumer<K, V> consumer, ConsumerRecords<K, V> records);
 
     void onConsumeFailure(KafkaConsumer<K, V> consumer, ConsumerRecords<K, V> records, ConsumeCallback<K, V> callback);
